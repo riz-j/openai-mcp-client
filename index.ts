f6618afde1da231889db110a1857e0431a53d32b
riz-j <adiputra979@gmail.com>
2025-04-10 18:29:57 +1000

    prog

--- index.ts
+++ index.ts
@@ -6,19 +6,19 @@ import OpenAI from "openai";
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 const transport = new StdioClientTransport({
-  command: "bun",
-  args: ["/home/rizki/Code/mcp/apps/calculator/server.js"]
+	command: "bun",
+	args: ["/home/rizki/Code/mcp/apps/calculator/server.js"]
 });
 
 const client = new Client({
-  name: "openai-mcp-client",
-  version: "0.0.1"
+	name: "openai-mcp-client",
+	version: "0.0.1"
 });
 
 const openAiClient = new OpenAiClient({
-  provider: openai,
-  client,
-  transport,
+	provider: openai,
+	client,
+	transport,
 });
 
 await openAiClient.connect();
@@ -26,10 +26,12 @@ await openAiClient.connect();
 // console.log(openAiClient.tools);
 
 const completion = await openai.chat.completions.create({
-  model: "gpt-4o-mini",
-  messages: [
-    { role: "user", content: "Hello!" },
-  ],
-  tools: openAiClient.tools,
-  tool_choice: "auto",
+	model: "gpt-4o-mini",
+	messages: [
+		{ role: "user", content: "Hello! Add 5 with 25" },
+	],
+	tools: openAiClient.tools,
+	tool_choice: "auto",
 });
+
+console.log(JSON.stringify(completion.choices, null, 2));
